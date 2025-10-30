d576561ed8ebbdc3d1efc841c7d3440298be165b
Cristin O’Connor <her@cristin.io>
2025-10-30 14:49:33 -0600

    Pre-deploy with Vercel

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+import InvoiceStatus from "./status"
+
+export default function Page() {
+  return <><p>Invoices Page</p>
+  <InvoiceStatus status="paid" /></>
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+// You'll use this file to keep the fonts that will be used throughout your application.
+
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
